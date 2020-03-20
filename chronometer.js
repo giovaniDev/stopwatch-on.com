7f22253dfb0f30f3f40b068d76573db2c55e2ffd
Giovani <giovani.rdgs@gmail.com>
2020-03-20 20:02:43 -0300

    refactoring javascript stopwatch and some changes

--- chronometer.js
+++ chronometer.js
@@ -6,20 +6,15 @@
         function run() {
 
             const interval = setInterval(() => {
+                document.querySelector('#stopwatch').innerHTML = `${minute}: ${second}: ${centesimal}`
                 centesimal += 1
-                document.querySelector('#centesimal').innerHTML = centesimal
                 if (centesimal === 100) {
                     second += 1
-                    document.querySelector('#second').innerHTML = second
                     centesimal = 0
-                    
                 }
                 if (second === 10) {        
                     minute += 1
-                    document.querySelector('#minute').innerHTML = minute
-                    second = 0
-                    document.querySelector('#second').innerHTML = second
-                    
+                    second = 0 
                 }
                 if (isActive === false) {
                     document.querySelector('#run').innerHTML = 'Run'
@@ -41,7 +36,5 @@
             centesimal = 0
             second = 0
             minute = 0
-            document.querySelector('#minute').innerHTML = minute
-            document.querySelector('#second').innerHTML = second
-            document.querySelector('#centesimal').innerHTML = centesimal
+            document.querySelector('#stopwatch').innerHTML = `${minute}: ${second}: ${centesimal}`
         }
